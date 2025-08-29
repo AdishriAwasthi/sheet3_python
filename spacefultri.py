91e1746eb8845343a905def42db1adedc3698d6a
AdishriAwasthi <awasthiadii78@gmail.com>
2025-08-29 08:48:55 +0530

    spacefultri.py

--- spacefultri.py
+++ spacefultri.py
@@ -2,8 +2,8 @@ n=int(input("enter a number : "))
 for i in range (1, n+1):
     for j in range (n-i):
         print("-", end =" ")
-    for j  in range (2*i-1):
-        print("*",end=" ")
+    for j  in range (i):
+        print("*  ",end=" ")
     print()
 
 # OUTPUT
